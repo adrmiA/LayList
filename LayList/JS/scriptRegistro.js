dd7c4027f06ac1b716884d5f8283890a58c6e8b9
Adriana Bonilla <adrmi.arc@gmail.com>
2025-03-02 22:22:13 -0600

    Update scriptRegistro.js

--- LayList/JS/scriptRegistro.js
+++ LayList/JS/scriptRegistro.js
@@ -1,13 +1,13 @@
-document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
-document.getElementById("btn__registrarse").addEventListener("click", register);
+document.getElementById("btn_iniciar-sesion").addEventListener("click", iniciarSesion);
+document.getElementById("btn_registrarse").addEventListener("click", register);
 window.addEventListener("resize", anchoPage);
 
 //Declarando variables
-var formulario_login = document.querySelector(".formulario__login");
-var formulario_register = document.querySelector(".formulario__register");
-var contenedor_login_register = document.querySelector(".contenedor__login-register");
-var caja_trasera_login = document.querySelector(".caja__trasera-login");
-var caja_trasera_register = document.querySelector(".caja__trasera-register");
+var formulario_login = document.querySelector(".formulario_login");
+var formulario_register = document.querySelector(".formulario_register");
+var contenedor_login_register = document.querySelector(".contenedor_login-register");
+var caja_trasera_login = document.querySelector(".caja_trasera-login");
+var caja_trasera_register = document.querySelector(".caja_trasera-register");
 
     //FUNCIONES
 
